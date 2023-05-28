4818dd856918069abef9b3c3a8bb4a4698c56def
htetsoemoe <soemoehtet.mdy@gmail.com>
2023-05-28 11:08:44 +0630

    finished project setup and configure component routes

---
+++ react-jwt-auth/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Path from './routes/Path'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-2xl font-semibold text-center'>JWT Authentication</h1>
+      <Path />
+    </div>
+  )
+}
+
+export default App
